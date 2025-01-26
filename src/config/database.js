03db5a3caf338f45eb52c68c62aa15150358b7fa
Felipe Rodrigues <felipe.rod00@hotmail.com>
2025-01-26 14:16:42 -0300

    teste2

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,8 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
   dialect: 'mariadb',
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
+  dialect: 'mariadb',
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'digital_wallet',

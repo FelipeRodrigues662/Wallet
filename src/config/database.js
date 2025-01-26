fcd983eea9feb76f3ce119f30851274903908cbe
Felipe Rodrigues <felipe.rod00@hotmail.com>
2025-01-26 14:55:10 -0300

    teste

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'digital_wallet',
+  port:3306,
   logging: false
 });
 
